1acafe718ed56b5ab4f059fb6a5d6e6f063e3b76
niaaluf <niaaudita01@gmail.com>
2022-11-13 17:38:48 +0700

    controller

--- src/main/java/pwsc/project4/controller.java
+++ src/main/java/pwsc/project4/controller.java
@@ -12,6 +12,7 @@ import java.util.Date;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,19 +25,23 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class controller {
     
-    @ResponseBody
+  
     @RequestMapping ("/getData")
     public String getData(@RequestParam("nama")String text,
                           @RequestParam("tanggal")@DateTimeFormat(pattern="yyyy-MM-dd") Date date,
-                          @RequestParam("image")MultipartFile file)
+                          @RequestParam("image")MultipartFile file, Model model)
                           throws IOException, ParseException {
         
-        SimpleDateFormat tanggal = new SimpleDateFormat("EE/dd/MMMM/yyyy");
-        
-        //membuat variable baru
-        String newTanggal = tanggal.format(date);
         String blob = Base64.encodeBase64String(file.getBytes());
+        String gambar = "data:image/jpeg;base64,".concat(blob);
         
-        return text + "<br><img widht 100 <img src='data:image/jpeg;base64, "+blob+ "'/> <br>"+newTanggal;
+        SimpleDateFormat tanggal = new SimpleDateFormat("EE/dd/MMMM/yyyy");
+        String newTanggal = tanggal.format(date);
+       
+        model.addAttribute("name", text);
+        model.addAttribute("tgl", newTanggal);
+        model.addAttribute("gambar", gambar);
+        
+        return "tampilan";
     }
 }
